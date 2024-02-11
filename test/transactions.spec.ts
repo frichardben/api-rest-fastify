a30d651ed20f72aaa8db8c9592e62ba0e8e20469
Richard Ben <frichardben@yahoo.com.br>
2024-02-11 11:46:18 -0300

    test: added list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { it, describe, beforeAll, afterAll } from 'vitest'
+import { it, describe, beforeAll, afterAll, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/server'
 
@@ -17,5 +17,29 @@ describe('Transactions', () => {
             amount: 1000,
             type: 'credit'
         }).expect(201)
+    })
+
+    it('should be abled to list all transactions', async () => {
+        const createTransactionResponse =  await request(app.server).post('/transactions').send({
+            title: 'New Transaction',
+            amount: 1000,
+            type: 'credit'
+        })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+       const listTransactionResponse =  await request(app.server)
+        .get('/transactions')
+        .send()
+        .set('Cookie', cookies)
+
+    
+       expect(listTransactionResponse.body.transactions).toEqual([
+        expect.objectContaining({
+            title: 'New Transaction',
+            amount: 1000,
+        })
+       ])
+
     })
 })
