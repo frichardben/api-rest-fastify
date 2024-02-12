63ed826992f31c187979bca757444be0d013ae81
Richard Ben <frichardben@yahoo.com.br>
2024-02-12 11:20:17 -0300

    test: get summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -81,6 +81,34 @@ describe('Transactions', () => {
 
     })
 
-  
+    it('should be able to get the summary.', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+            title: 'New Transaction',
+            amount: 1000,
+            type: 'credit'
+        })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        await request(app.server)
+            .post('/transactions')
+            .set('Cookie', cookies)
+            .send({
+                title: 'Debit Transaction',
+                amount: 400,
+                type: 'debit'
+            })
+
+       const summaryResponse = await request(app.server)
+        .get('/transactions/summary')
+        .set('Cookie', cookies)
+    
+       expect(summaryResponse.body.summary).toEqual([{
+        amount: 600
+       }])
+
+    })
  
 })
